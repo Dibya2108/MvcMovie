ba2d8fb7cc267259ec490ba313956ba61acc33f0
Dibyashri Mukherjee <dibya0508@gmail.com>
2023-12-21 16:34:50 +0530

    ..

--- FourthWebApp/Controllers/ShowTimeController.cs
+++ FourthWebApp/Controllers/ShowTimeController.cs
@@ -229,34 +229,45 @@ namespace MvcMovie.Controllers
 
         private string GetPrintTicketPdf(int id)
         {
-            ShowTimeViewModel view = new ShowTimeViewModel();
-            view = _ShowTimeDalSql.GetBookingsInfo(id);
+            ShowTimeViewModel view = _ShowTimeDalSql.GetBookingsInfo(id);
 
             StringBuilder sb = new StringBuilder();
-
 
             sb.Append("<div><img src='" + view.MovieImage + "' style='width: 100px; height: 100px;'/></div>");
 
             sb.Append("-------------------------------------------------------------------------------------------------------------------------------");
 
             sb.Append("<div style='text-align:left; font-size:30px;'><b>" + view.Title + "</b></div>");
-            sb.Append("<div>"+ "( " + view.LanguageName + " )"+ "</div>");
-            sb.Append("-------------------------------------------------------------------------------------------------------------------------------");
-            sb.Append("<table style='width:100%'><tr><td style='text-align:left; font-size:18px;'>" + "Date" + "</td><td style='text-align:center; font-size:18px;'>" + "Time" + "</td></tr></table>");
-            sb.Append("<table style='width:100%'><tr><td style='text-align:left; font-size:18px; font-weight:bold;'>" + view.ShowDate.ToString("ddd, dd MMM, yyyy") + "</td><td style='text-align:center; font-size:18px; font-weight:bold;'>" + view.ShowTime + "</td></tr></table>");
-            
+            sb.Append("<div>" + "( " + view.LanguageName + " )" + "</div>");
             sb.Append("-------------------------------------------------------------------------------------------------------------------------------");
 
-            sb.Append("<table style='width:100%'><tr><td style='text-align:left; font-size:18px;'>" + "Seats" + "</td><td style='text-align:center; font-size:18px;'>" + "No. ofTicket" + "</td></tr></table>");
-            sb.Append("<table style='width:100%'><tr><td style='text-align:left; font-size:25px; font-weight:bold;'>" + view.TypeName.PadRight(30) + "</td><td style='font-size:25px; font-weight:bold;'>" + view.NoOfTicket + "</td></tr></table>");
+            sb.Append("<table style='width:100%'>");
+            sb.Append("<tr><td style='text-align:left; font-size:18px;'>" + "Date" + "</td>");
+            sb.Append("<td style='text-align:center; font-size:18px;'>" + "Time" + "</td></tr>");
+            sb.Append("</table>");
 
-            //sb.Append("<div><b>" + view.ShowTime + "</b></div>");
+            sb.Append("<table style='width:100%'>");
+            sb.Append("<tr><td style='text-align:left; font-size:18px; font-weight:bold;'>" + view.ShowDate.ToString("ddd, dd MMM, yyyy") + "</td>");
+            sb.Append("<td style='text-align:center; font-size:18px; font-weight:bold;'>" + view.ShowTime + "</td></tr>");
+            sb.Append("</table>");
 
+            sb.Append("-------------------------------------------------------------------------------------------------------------------------------");
+
+            sb.Append("<table style='width:100%'>");
+            sb.Append("<tr><td style='text-align:left; font-size:18px;'>" + "Seats" + "</td>");
+            sb.Append("<td style='text-align:center; font-size:18px;'>" + "No. of Tickets" + "</td></tr>");
+            sb.Append("</table>");
+
+            sb.Append("<table style='width:100%'>");
+            sb.Append("<tr><td style='text-align:left; font-size:25px; font-weight:bold;'>" + view.TypeName + "</td>");
+            sb.Append("<td style='font-size:25px; font-weight:bold;'>" + view.NoOfTicket + "</td></tr>");
+            sb.Append("</table>");
 
             return sb.ToString();
         }
 
-        
+
+
 
         public string DeleteShow(int showTimeId)
         {
